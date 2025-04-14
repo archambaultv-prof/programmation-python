b545c3e668d0c4aa63f5033319fb051f9b6c5cec
Vincent Archambault-Bouffard <varchambaultbouffard@cmaisonneuve.qc.ca>
2025-04-14 19:56:45 -0400

    Update front page. Delete test page.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,58 +4,60 @@ import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: "Cours 420-2C3-MA",
-    Svg: require("@site/static/img/2C3.svg").default,
-    description: (
-      <>
-        Pour les informations spécifiques à la session d’hiver 2025, les
-        étudiants du{" "}
-        <a
-          href="https://www.cmaisonneuve.qc.ca/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Collège de Maisonneuve{" "}
-        </a>
-        peuvent consulter{" "}
-        <a
-          href="https://archambaultv-prof.github.io/2025H-420-2C3-MA/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          le site dédié.
-        </a>
-      </>
-    ),
-  },
-  {
     title: "Pour plus de spam et d'hirondelles",
+    titleLink: "https://www.youtube.com/user/MontyPython",
     Svg: require("@site/static/img/Monty-Python.svg").default,
     description: (
       <>
         Python tient son nom de la troupe de comédiens britanniques les Monty
         Python. Lorsque vous êtes tanné de lire les notes de cours, vous pouvez
-        consulter leur{" "}
-        <a
-          href="https://www.youtube.com/user/MontyPython"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           page YouTube officielle.
-        </a>
+        consulter leur page youtube.
+      </>
+    ),
+  },
+  {
+    title: "Cours 420-2C3-MA",
+    titleLink: "https://archambaultv-prof.github.io/2025H-420-2C3-MA/",
+    Svg: require("@site/static/img/2C3.svg").default,
+    description: (
+      <>
+      Site web pour les informations spécifiques du cours 420-2C3-MA à la session
+      d'hiver 2025.
+      </>
+    ),
+  },
+  {
+    title: "Cours 420-910-MA",
+    titleLink: "https://archambaultv-prof.github.io/2025H-420-910-MA/",
+    Svg: require("@site/static/img/910.svg").default,
+    description: (
+      <>
+      Site web pour les informations spécifiques du cours de la formation continue 420-910-MA débutant en avril 2025.
       </>
     ),
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ Svg, title, titleLink, description }) {
   return (
     <div className={clsx("col col--6")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        {titleLink ? (
+          <a href={titleLink}>
+            <Svg className={styles.featureSvg} alt={title} />
+          </a>
+        ) : (
+          <Svg className={styles.featureSvg} alt={title} />
+        )}
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        {titleLink ? (
+          <Heading as="h3">
+            <a href={titleLink}>{title}</a>
+          </Heading>
+        ) : (
+          <Heading as="h3">{title}</Heading>
+        )}
         <p>{description}</p>
       </div>
     </div>
